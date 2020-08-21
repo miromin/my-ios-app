6d874cafe4c76b2358b15b8be12e6fc14936180f
miro <miroslav@luno.com>
2020-08-21 08:02:06 +0200

    Initial Commit

---
+++ my_ios_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  my_ios_app
+//
+//  Created by Miroslav Minev on 2020/08/21.
+//  Copyright © 2020 Miroslav Minev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ my_ios_appTests/my_ios_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  my_ios_appTests.swift
+//  my_ios_appTests
+//
+//  Created by Miroslav Minev on 2020/08/21.
+//  Copyright © 2020 Miroslav Minev. All rights reserved.
+//
+
+import XCTest
+@testable import my_ios_app
+
+class my_ios_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
